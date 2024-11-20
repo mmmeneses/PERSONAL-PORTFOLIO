9d1ec5b8f6353e680911cc4a386e56d1bf2fc56a
Mauricio Meneses <mmenesesmiller@gmail.com>
2024-11-20 22:42:47 +0000

    I have created the js file in which I have written the code that contains the conditions that makes the backgorund and text of the website change when it meets the time condition.

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+function updateBackgroundColor() {
+    const currentHour = new Date().getHours();
+    const body = document.body;
+    const textElements = document.querySelectorAll('h1, h2, h3, p, a, footer'); // selecciona los elementos de texto
+
+    if(currentHour >= 18 || currentHour < 6) {
+        
+        // de 6 PM a 12 AM y de 12 AM a 6AM
+        
+        body.style.backgroundColor = 'midnightblue';
+        textElements.forEach(element => {
+            element.style.color = 'white'; // cambia el texto a blanco
+        });
+    } else {
+        // De 6 AM a 6 PM
+
+        body.style.backgroundColor = '#f8f870';
+        textElements.styleforEach(element => {
+            element.style.color = 'black'; // cambia el color del texto a negro
+        });
+    }
+}
+
+// Actualiza el fondo y el texto cuando se carga la pagina
+
+updateBackgroundColor();
+
+// Configura un temporizador para verificar cada minuto
+
+setInterval(updateBackgroundColor, 60000);
